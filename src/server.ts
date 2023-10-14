761bb58159c2464137643714a162b26160f5a4c0
narumir <narumir@narumir.io>
2023-10-14 21:47:44 +0000

    apply graceful shutdown

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,28 @@
 import http from "http";
 import {
+  promisify,
+} from "util";
+import {
   app,
 } from "./app";
 
 const server = http.createServer(app);
+const closeServer = promisify(server.close);
 const port = 4000;
+const shutdown = async () => {
+  try {
+    await closeServer();
+  } catch (e) {
+    console.error(e);
+  } finally {
+    process.kill(process.pid);
+  }
+};
 
 server.listen(port, () => {
   console.info(`The application started at ${port}`);
 });
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+process.on("SIGUSR2", shutdown);
